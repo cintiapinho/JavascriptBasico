62c73bfaeb2c0093325932bf467b81a4053f63f2
Cíntia Pinho <30634441+cintiapinho@users.noreply.github.com>
2023-08-01 11:02:50 -0300

    Add files via upload

---
+++ aula1Declararvariaveis/variaveis.js
@@ -0,0 +1,14 @@
+x=2;
+y=3.5;
+z=7;
+s1="Campanha Avon";
+s2=" do ";
+s3="JavaScript";
+s=s1+s2+s3;                 // concatenação de strings
+soma=x+y;                   // adição
+mult=x*y;                   // multiplicação
+div=x/y;                    // divisão
+sub=x-y;                    // subtracção
+mod=x%y;
+pontos = 100; 
+nome ="Fulano de Tal"                   // resto da divisão inteira
